1a03f7071b1a70dc656ef68d2270dfef01b20107
Carlose <carlosviniwork@gmail.comm>
2021-10-04 23:57:14 -0300

    inicio de desenvolvimento da aplicação ecomerce

---
+++ src/components/data/dataBase.js
@@ -0,0 +1,24 @@
+import Products from './products.json'
+import './styleData.css'
+
+export function Games() {
+/*   function AddShoppingList(id){
+    console.log(`passei por aqui pelo id${id}`)
+  } */
+  return (
+    <div className="styleComponentGames">
+      { Products.map(post => { 
+        return(
+          <div className="cards">
+              <h1>{ post.name}</h1>
+              <img alt={post.image} src={require(`../../assets/img-games/${post.image}`).default} />
+              <div className="priceAndButton">
+                <h2>{ post.price } $</h2>
+                <button onClick={  }>+</button>
+              </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+};
